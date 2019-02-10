25ea989d7885e7151eff82e0869e5d91b730e919
x1QG1x <x1q.g1x@gmail.com>
2019-02-10 00:34:55 +0000

    fix(routing): default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './guards/auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'loot-tracker', pathMatch: 'full' },
   { path: 'loot-tracker', loadChildren: './loot-tracker/loot-tracker.module#LootTrackerModule', canActivate: [AuthGuard]},
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
 ];
